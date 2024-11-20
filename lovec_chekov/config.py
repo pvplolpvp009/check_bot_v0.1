cdc76a036578d2a67fb6ec5bd7fa4c95da4e8476
pvplolpvp009 <64464398+pvplolpvp009@users.noreply.github.com>
2024-11-20 15:15:20 +0300

    Add files via upload

---
+++ lovec_chekov/config.py
@@ -0,0 +1,19 @@
+api_id = "28772126" #айпи айди
+api_hash = '7e00cf98a33353e0e622d1f9c6e0eafd' #айпи ключ
+#: https://telegra.ph/Sozdayom-paru-api-idhash-dlya-lovca-chekov-11-03
+
+channel = '-1002294998102' #каналы чтоб с тегом 'без @' брать цыфери https://t.me/SimpleID_Bot просто перекинуть соо с канала
+
+
+
+avto_vivod = False # Если данные параметр True, то скрипт раз в сутки будет переводить деньги с помощью чека на указанный аккаунт. Чтобы отключить укажите False, например avto_vivod = False
+avto_vivod_tag = 'pvplolpvp009' # Тег аккаунта(без @), куда раз в сутки будет отправляться чек со всеми собранными средствами. Например avto_vivod_tag = 'absolutely_enough'
+
+avto_otpiska = False # онли False
+
+anti_captcha = True #Капча онли True
+
+ocr_api_key = 'K88919780688957' #https://telegra.ph/Poluchenie-OCR-API-KEY-11-03
+
+
+
